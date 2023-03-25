ef0234836f179c0015a78fa94cb470518e4f87c0
Roman Molotov <r.molotov@yahoo.com>
2023-03-25 19:26:33 +0400

    addressables provider

---
+++ Assets/PerelesoqTest/Infrastructure/AssetManagement/IAssetProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+
+namespace PerelesoqTest.Infrastructure.AssetManagement
+{
+    public interface IAssetProvider
+    {
+        public Task<T> Load<T>(string key) where T : class;
+        public Task<SceneInstance> LoadScene(string sceneName, LoadSceneMode mode = LoadSceneMode.Single);
+        public void Release(string key);
+        public void Cleanup();
+    }
+}
