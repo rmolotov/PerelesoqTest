e6323738ae5b94b191e5045b079ac3e3e05f8e2b
Roman Molotov <r.molotov@yahoo.com>
2023-03-27 12:46:36 +0400

    input and output port components for gadgets, wireup its in rooms

---
+++ Assets/PerelesoqTest/Gameplay/Gadgets/Ports/InputPort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PerelesoqTest.Gameplay.Gadgets.Ports
+{
+    public class InputPort : MonoBehaviour
+    {
+        public List<OutputPort> Inputs;
+        public event Action<int> CurrentChanged;
+
+        private readonly List<Action> _inputHandlers = new();
+
+        private void Awake()
+        {
+            foreach (var input in Inputs)
+            {
+                void Handler() => CurrentChanged?.Invoke(Inputs.IndexOf(input));
+                _inputHandlers.Add(Handler);
+
+                input.CurrentChanged += Handler;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            for (var index = 0; index < _inputHandlers.Count; index++)
+                Inputs[index].CurrentChanged -= _inputHandlers[index];
+            
+            _inputHandlers.Clear();
+        }
+    }
+}
---
+++ Assets/PerelesoqTest/Gameplay/Gadgets/Ports/OutputPort.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace PerelesoqTest.Gameplay.Gadgets.Ports
+{
+    [DisallowMultipleComponent]
+    public class OutputPort : MonoBehaviour
+    {
+        public int Current;
+        public event Action CurrentChanged;
+    }
+}
