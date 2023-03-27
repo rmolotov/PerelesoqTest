125bb016507e71f2d9917386ee02d6bcea68fee8
Roman Molotov <r.molotov@yahoo.com>
2023-03-27 19:33:48 +0400

    connectors: base and bridge

---
+++ Assets/PerelesoqTest/Gameplay/Gadgets/Connectors/Bridge.cs
@@ -0,0 +1,11 @@
+﻿namespace PerelesoqTest.Gameplay.Gadgets.Connectors
+{
+    public class Bridge : ConnectorBase
+    {
+        protected override void ChangeOutputCurrent()
+        {
+            outputPort.Current = inputPort.Inputs[0].Current;
+            base.ChangeOutputCurrent();
+        }
+    }
+}
---
+++ Assets/PerelesoqTest/Gameplay/Gadgets/Connectors/ConnectorBase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using PerelesoqTest.Gameplay.Gadgets.Ports;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace PerelesoqTest.Gameplay.Gadgets.Connectors
+{
+    [RequireComponent(typeof(InputPort))]
+    [RequireComponent(typeof(OutputPort))]
+    public class ConnectorBase : MonoBehaviour
+    {
+        [BoxGroup("Connections")] [SerializeField]
+        protected InputPort inputPort;
+        [BoxGroup("Connections")] [SerializeField]
+        protected OutputPort outputPort;
+
+        [InfoBox("In most cases indicators count should be equal to count of input ports. Please set them up on the level scene!", 
+            InfoMessageType.Warning, 
+            "@inputIndicatorsMeshes.Count != inputPort.Inputs.Count || inputIndicatorsMeshes.Count == 0")]
+        [BoxGroup("Indicators")] [SerializeField]
+        protected List<MeshRenderer> inputIndicatorsMeshes;
+        [BoxGroup("Indicators")] [SerializeField]
+        protected MeshRenderer outputIndicatorMesh;
+        [BoxGroup("Indicators")] [SerializeField]
+        protected Material onMaterial, offMaterial;
+
+        private Action<int> CurrentHandler;
+
+        protected virtual void OnEnable()
+        {
+            inputPort.CurrentChanged += CurrentHandler = _ => ChangeOutputCurrent();
+            inputPort.CurrentChanged += ChangeInputIndicatorMaterial;
+
+            outputPort.CurrentChanged += ChangeOutputIndicatorMaterial;
+        }
+
+        protected virtual void OnDisable()
+        {
+            inputPort.CurrentChanged -= CurrentHandler;
+            inputPort.CurrentChanged -= ChangeInputIndicatorMaterial;
+            
+            outputPort.CurrentChanged -= ChangeOutputIndicatorMaterial;
+        }
+
+        protected virtual void ChangeOutputCurrent() => 
+            outputPort.CurrentChanged?.Invoke();
+
+        private void ChangeInputIndicatorMaterial(int index)
+        {
+            if (index < inputIndicatorsMeshes.Count)
+                inputIndicatorsMeshes[index].sharedMaterial = inputPort.Inputs[index].Current > 0
+                    ? onMaterial
+                    : offMaterial;
+        }
+
+        private void ChangeOutputIndicatorMaterial() =>
+            outputIndicatorMesh.sharedMaterial = outputPort.Current > 0
+                ? onMaterial
+                : offMaterial;
+    }
+}
