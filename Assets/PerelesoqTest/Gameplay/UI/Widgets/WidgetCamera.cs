9cb34a42b9347fa3aff583982f433b006cd42c64
Roman Molotov <r.molotov@yahoo.com>
2023-03-29 14:34:47 +0400

    widgets: camera

---
+++ Assets/PerelesoqTest/Gameplay/UI/Widgets/WidgetCamera.cs
@@ -0,0 +1,21 @@
+﻿using PerelesoqTest.Gameplay.Gadgets;
+using UnityEngine;
+using UnityEngine.UI;
+using Camera = PerelesoqTest.Gameplay.Gadgets.Functions.Camera;
+
+namespace PerelesoqTest.Gameplay.UI.Widgets
+{
+    public class WidgetCamera : WidgetBase
+    {
+        [SerializeField] private Button selectButton;
+
+        public override void Initialize(GadgetBaseInfo gadgetInfo)
+        {
+            var camera = gadgetInfo.GetComponent<Camera>();
+            
+            selectButton.onClick.AddListener(() => camera.Interact());
+            
+            base.Initialize(gadgetInfo);
+        }
+    }
+}
